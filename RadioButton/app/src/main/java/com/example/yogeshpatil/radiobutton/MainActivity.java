dac8ff7ea69700d809ff7ebacf1ab861ed455959
yogeshdp <pyogesh092@gmail.com>
2016-08-22 17:25:16 +0530

    radio button done

---
+++ RadioButton/app/src/main/java/com/example/yogeshpatil/radiobutton/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.yogeshpatil.radiobutton;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private RadioGroup radioSexGroup;
+    private RadioButton radioSexButton;
+    private Button btnDisplay;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        radioSexGroup = (RadioGroup) findViewById(R.id.radioSex);
+        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioSexGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioSexButton = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(MainActivity.this,
+                        radioSexButton.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+    }
+}
